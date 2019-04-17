90c5bcdc97ea060fe28c4c8cb68be266f113789c
Isaac Pinheiro <isaacpnhr@gmail.com>
2019-04-17 19:09:14 -0300

    Atualizando a versão do Driver do MySQL.

--- src/main/java/br/edu/ufabc/sucupirafilter/config/ConnectionFactory.java
+++ src/main/java/br/edu/ufabc/sucupirafilter/config/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
     public Connection getConnection()
         throws SQLException, ClassNotFoundException {
 
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://localhost/sucupirafilter";
         return DriverManager.getConnection(url, "sucupirafilter", "senha");
 
