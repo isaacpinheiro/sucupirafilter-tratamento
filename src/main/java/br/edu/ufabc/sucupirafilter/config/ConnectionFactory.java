817ac5bc9791deca11cddf0b37cc18a34f53faf8
Isaac Pinheiro <isaacpnhr@gmail.com>
2019-04-16 22:26:47 -0300

    Adicionando o ConnectionFactory.

---
+++ src/main/java/br/edu/ufabc/sucupirafilter/config/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.edu.ufabc.sucupirafilter.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public Connection getConnection()
+        throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost/sucupirafilter";
+        return DriverManager.getConnection(url, "sucupirafilter", "senha");
+
+    }
+
+}
+
